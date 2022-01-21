514a99e718bf96141703243f725a5f4add8ce095
nedosika <nedosika@gmail.com>
2022-01-21 16:12:43 +0200

    add update adn remove

--- controllers/film.js
+++ controllers/film.js
@@ -5,15 +5,21 @@ import Film from "../models/Film.js"
 const add = async (req, res) => {
     try {
         const data = req.body;
-        const filmRef = db.collection('films').doc().set(data);
+        const filmRef = db.collection('films').doc();
 
         await filmRef.set(data);
 
         res.status(201).send({
-            data: {
-                ...data,
-                id: filmRef.id
-            },
+            data: new Film(
+                filmRef.id,
+                data.name,
+                data.year,
+                data.rating,
+                data.genre,
+                data.link,
+                data.torrentLink,
+                data.status
+            ),
             status: "added"
         });
     } catch (error) {
@@ -24,16 +30,26 @@ const add = async (req, res) => {
 const getOne = async (req, res) => {
     try {
         const id = req.params.id;
-        const film = await db.collection('films').doc(id).get();
+        const filmRef = await db.collection('films').doc(id);
+        const doc = await filmRef.get();
 
-        if (!film.exists) {
-            res.status(204).send({
-                message: "student not found",
+        if (!doc.exists) {
+            res.status(404).send({
+                message: "film not found",
                 status: "not found"
             })
         } else {
             res.status(200).send({
-                data: film.data(),
+                data: new Film(
+                    id,
+                    doc.data().name,
+                    doc.data().year,
+                    doc.data().rating,
+                    doc.data().genre,
+                    doc.data().link,
+                    doc.data().torrentLink,
+                    doc.data().status
+                ),
                 status: "ok"
             })
         }
@@ -46,12 +62,17 @@ const getAll = (req, res) => {
     return db.collection('films').get()
         .then((snapshot) => {
             if (snapshot.empty) {
-                res.status(204).send({message: "films not found", status: 'not found'});
+                res.status(404).send({
+                    data: {
+                        message: "films not found "
+                    },
+                    status: 'not found'
+                });
             } else {
                 const films = [];
 
                 snapshot.forEach((doc) => {
-                    const film = new Film(
+                    films.push(new Film(
                         doc.id,
                         doc.data().name,
                         doc.data().year,
@@ -60,11 +81,13 @@ const getAll = (req, res) => {
                         doc.data().link,
                         doc.data().torrentLink,
                         doc.data().status
-                    )
-                    films.push(film);
+                    ));
                 })
 
-                res.status(200).send({data: films, status: 'ok'});
+                res.status(200).send({
+                    data: films,
+                    status: 'ok'
+                });
             }
         })
         .catch((err) => {
@@ -75,20 +98,25 @@ const getAll = (req, res) => {
 const update = async (req, res) => {
     try {
         const id = req.params.id;
+        const filmRef = await db.collection('films').doc(id);
+        const film = await filmRef.get();
 
-        await db.collection('films').doc(id).update(req.body);
+        if (!film.exists) {
+            res.status(404).send({
+                message: "film not found",
+                status: "not found"
+            })
+        } else {
+            await filmRef.update(req.body);
 
-        res.status(200).send({
-            data: {
-                id
-            },
-            status: "updated"
-        });
-
+            res.status(200).send({
+                data: film.data(),
+                status: "updated"
+            });
+        }
     } catch (error) {
         res.status(400).send(error.message);
     }
-
 }
 
 const remove = async (req, res) => {
--- routes/film.js
+++ routes/film.js
@@ -7,6 +7,7 @@ const router = express.Router();
 router.post('/films', filmController.add);
 router.get('/films', filmController.getAll);
 router.get('/films/:id', filmController.getOne);
+router.put('/films/:id', filmController.update);
 router.delete('/films/:id', filmController.remove);
 
 export default {
