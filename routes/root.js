2efa5f6a3f91a4f04c29fbc2f999f91e59e1a34d
Pavel <nedosika@gmail.com>
2022-01-31 00:08:36 +0200

    add middleware auth

--- routes/root.js
+++ routes/root.js
@@ -1,10 +1,14 @@
 import express from "express";
+import auth from "../middleware/auth.js"
 
 import rootController from "../controllers/root.js";
 
 const router = express.Router();
 
 router.get("/", rootController.helloWorld);
+router.post("/welcome", auth, (req, res) => {
+  res.status(200).send("Welcome 🙌 ");
+});
 
 export default {
   routes: router
