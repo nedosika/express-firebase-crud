9aa79a2a00206c3b92f81f005e8bd557555704d0
Pavel Nedosika <nedosika@mail.ru>
2022-01-31 22:19:19 +0200

    change signin signup responses

--- controllers/auth.js
+++ controllers/auth.js
@@ -6,72 +6,87 @@ import User from "../models/User.js";
 import config from "../config.js";
 
 const signIn = async (req, res) => {
-    try {
-        const {email, password} = req.body;
+  try {
+    const { email, password } = req.body;
 
-        if (!(email && password)) {
-            res.status(400).send("All input is required");
-        }
-
-        const user = await User.getOne(email);
-
-        if (user && (await bcrypt.compare(password, user.password))) {
-            const token = jwt.sign(
-                { user_id: user.id, email },
-                config.jwtTokenKey,
-                {
-                    expiresIn: "2h",
-                }
-            );
-
-            user.token = token;
-
-            res.status(200).json(user);
-        }
-        res.status(400).send("Invalid Credentials");
-    } catch (error) {
-        console.log(error.message);
+    if (!(email && password)) {
+      res.status(400).send({
+        message: "All input is required",
+        status: "Required"
+      });
     }
-}
+
+    const user = await User.getOne(email);
+
+    if (user && (await bcrypt.compare(password, user.password))) {
+      const token = jwt.sign({ user_id: user.id, email }, config.jwtTokenKey, {
+        expiresIn: "2h"
+      });
+      console.log(user);
+      return res.status(200).send({
+        data: {
+          ...user,
+          token
+        },
+        status: "OK"
+      });
+    }
+
+    res.status(400).send({
+      message: "Invalid Credentials",
+      status: "Invalid"
+    });
+  } catch (error) {
+    console.log(error.message);
+  }
+};
 
 const signUp = async (req, res) => {
-    try {
-        const {email, password} = req.body;
+  try {
+    const { email, password } = req.body;
 
-        if (!(email && password)) {
-            res.status(400).send("All input is required");
-        }
-
-        const oldUser = await User.getOne(email);
-
-        if (oldUser) {
-            return res.status(409).send("User Already Exist. Please Login");
-        }
-
-        const encryptedPassword = await bcrypt.hash(password, 10);
-
-        const user = await User.create({
-            email: email.toLowerCase(),
-            password: encryptedPassword,
-        });
-
-        const token = jwt.sign(
-            {user_id: user.id, email},
-            config.jwtTokenKey,
-            {
-                expiresIn: "2h",
-            }
-        );
-
-        user.token = token;
-
-        res.status(201).json(user);
-    } catch (err) {
-        console.log(err);
+    if (!(email && password)) {
+      return res.status(400).send({
+        message: "All input is required",
+        status: "Required"
+      });
     }
-}
+
+    const oldUser = await User.getOne(email);
+
+    if (oldUser) {
+      return res.status(409).send({
+        message: "User Already Exist. Please Login",
+        status: "Exist"
+      });
+    }
+
+    const encryptedPassword = await bcrypt.hash(password, 10);
+
+    const user = await User.create({
+      email: email.toLowerCase(),
+      password: encryptedPassword
+    });
+
+    const token = jwt.sign({ user_id: user.id, email }, config.jwtTokenKey, {
+      expiresIn: "2h"
+    });
+
+    user.token = token;
+
+    res.status(201).send({
+      data: {
+        ...user,
+        token
+      },
+      status: "OK"
+    });
+  } catch (err) {
+    console.log(err);
+  }
+};
 
 export default {
-    signIn,
-    signUp
-}
+  signIn,
+  signUp
+};
--- controllers/film.js
+++ controllers/film.js
@@ -3,138 +3,138 @@ import _ from "lodash";
 import Film from "../models/Film.js";
 
 const add = async (req, res) => {
-    try {
-        const data = req.body;
+  try {
+    const data = req.body;
 
-        const film = await Film.create(data);
+    const film = await Film.create(data);
 
-        res.status(201).send({
-            data: film,
-            message: "Film added",
-            status: "Added"
-        });
-    } catch (error) {
-        res.status(500).send({
-            data: {},
-            message: error.message,
-            status: "Error"
-        });
-    }
+    res.status(201).send({
+      data: film,
+      message: "Film added",
+      status: "Added"
+    });
+  } catch (error) {
+    res.status(500).send({
+      data: {},
+      message: error.message,
+      status: "Error"
+    });
+  }
 };
 
 const getOne = async (req, res) => {
-    try {
-        const id = req.params.id;
+  try {
+    const id = req.params.id;
 
-        const film = await Film.getOne(id);
+    const film = await Film.getOne(id);
 
-        if (_.isEmpty(film)) {
-            res.status(404).send({
-                data: {},
-                message: "Film not found",
-                status: "not found"
-            });
-        } else {
-            res.status(200).send({
-                data: {id, ...film},
-                status: "ok"
-            });
-        }
-    } catch (error) {
-        res.status(500).send({
-            data: {},
-            message: error.message,
-            status: "Error"
-        });
+    if (_.isEmpty(film)) {
+      res.status(404).send({
+        data: {},
+        message: "Film not found",
+        status: "not found"
+      });
+    } else {
+      res.status(200).send({
+        data: { id, ...film },
+        status: "ok"
+      });
     }
+  } catch (error) {
+    res.status(500).send({
+      data: {},
+      message: error.message,
+      status: "Error"
+    });
+  }
 };
 
 const getAll = async (req, res) => {
-    try {
-        const films = await Film.getAll();
+  try {
+    const films = await Film.getAll();
 
-        if (films.length) {
-            res.status(200).send({
-                data: films,
-                message: "",
-                status: "ok"
-            });
-        } else {
-            res.status(404).send({
-                data: {},
-                message: "Films not found",
-                status: "Not found"
-            });
-        }
-    } catch (error) {
-        res.status(500).send({
-            data: {},
-            message: error.message,
-            status: "Error"
-        });
+    if (films.length) {
+      res.status(200).send({
+        data: films,
+        message: "",
+        status: "ok"
+      });
+    } else {
+      res.status(404).send({
+        data: {},
+        message: "Films not found",
+        status: "Not found"
+      });
     }
+  } catch (error) {
+    res.status(500).send({
+      data: {},
+      message: error.message,
+      status: "Error"
+    });
+  }
 };
 
 const update = async (req, res) => {
-    try {
-        const id = req.params.id;
-        const data = req.body;
+  try {
+    const id = req.params.id;
+    const data = req.body;
 
-        const film = await Film.update({id, ...data});
+    const film = await Film.update({ id, ...data });
 
-        if (_.isEmpty(film)) {
-            res.status(404).send({
-                data,
-                message: "Film not found",
-                status: "Not found"
-            });
-        } else {
-            res.status(200).send({
-                data: film,
-                message: "Film updated",
-                status: "Updated"
-            });
-        }
-    } catch (error) {
-        res.status(500).send({
-            data: {},
-            message: error.message,
-            status: "Error"
-        });
+    if (_.isEmpty(film)) {
+      res.status(404).send({
+        data,
+        message: "Film not found",
+        status: "Not found"
+      });
+    } else {
+      res.status(200).send({
+        data: film,
+        message: "Film updated",
+        status: "Updated"
+      });
     }
+  } catch (error) {
+    res.status(500).send({
+      data: {},
+      message: error.message,
+      status: "Error"
+    });
+  }
 };
 
 const remove = async (req, res) => {
-    try {
-        const id = req.params.id;
-        const film = await Film.remove(id);
+  try {
+    const id = req.params.id;
+    const film = await Film.remove(id);
 
-        if (_.isEmpty(film)) {
-            res.status(404).send({
-                data: {id},
-                message: "Film not found",
-                status: "Not found"
-            });
-        } else {
-            res.status(200).send({
-                data: film,
-                message: "Film removed",
-                status: "Removed"
-            });
-        }
-    } catch (error) {
-        res.status(500).send({
-            data: {},
-            message: error.message,
-            status: "Error"
-        });
+    if (_.isEmpty(film)) {
+      res.status(404).send({
+        data: { id },
+        message: "Film not found",
+        status: "Not found"
+      });
+    } else {
+      res.status(200).send({
+        data: film,
+        message: "Film removed",
+        status: "Removed"
+      });
     }
+  } catch (error) {
+    res.status(500).send({
+      data: {},
+      message: error.message,
+      status: "Error"
+    });
+  }
 };
 
 export default {
-    add,
-    getOne,
-    getAll,
-    update,
-    remove
+  add,
+  getOne,
+  getAll,
+  update,
+  remove
 };
