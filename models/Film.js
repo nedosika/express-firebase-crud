ce8de2b5f7deb02154bbc4cf3354f735a042069c
Pavel Nedosika <nedosika@mail.ru>
2022-02-05 09:37:25 +0200

    add update sb

--- models/Film.js
+++ models/Film.js
@@ -1,51 +1,46 @@
 import _ from "lodash";
 
-import firestore, {COLLECTIONS} from "../services/Firestore/index.js";
+import firestore, { COLLECTIONS } from "../services/Firestore/index.js";
 
 class Film {
-    constructor(id, name, year, rating, genre, link, torrentLink, status) {
-        this.id = id;
-        this.name = name;
-        this.year = year;
-        this.rating = rating;
-        this.genre = genre;
-        this.link = link;
-        this.torrentLink = torrentLink;
-        this.status = status;
-    }
+  constructor(id, name, year, rating, genre, link, torrentLink, status) {
+    this.id = id;
+    this.name = name;
+    this.year = year;
+    this.rating = rating;
+    this.genre = genre;
+    this.link = link;
+    this.torrentLink = torrentLink;
+    this.status = status;
+  }
 
-    static getOne = (id) =>
-        firestore.getDocOne(COLLECTIONS.films, id);
+  static getOne = (id) => firestore.getDocOne(COLLECTIONS.films, id);
 
-    static create = (film) =>
-        firestore.add(COLLECTIONS.films, film);
+  static create = (film) => firestore.add(COLLECTIONS.films, film);
 
-    static getAll = () =>
-        firestore.getDocAll(COLLECTIONS.films);
+  static getAll = () => firestore.getDocAll(COLLECTIONS.films);
 
-    static update = async (updatedFilm) => {
-        const oldFilm = await Film.getOne(updatedFilm.id);
+  static update = async (updatedFilm) => {
+    const oldFilm = await Film.getOne(updatedFilm.id);
 
-        if(_.isEmpty(oldFilm))
-            return;
+    if (_.isEmpty(oldFilm)) return;
 
-        const newFilm = {...oldFilm, ...updatedFilm};
+    const newFilm = { ...oldFilm, ...updatedFilm };
 
-        await firestore.update(COLLECTIONS.films, newFilm);
+    await firestore.update(COLLECTIONS.films, newFilm);
 
-        return newFilm;
-    };
+    return newFilm;
+  };
 
-    static remove = async (id) => {
-        const film = await Film.getOne(id);
+  static remove = async (id) => {
+    const film = await Film.getOne(id);
 
-        if(_.isEmpty(film))
-            return {};
+    if (_.isEmpty(film)) return {};
 
-        await firestore.remove(COLLECTIONS.films, id);
+    await firestore.remove(COLLECTIONS.films, id);
 
-        return film;
-    }
+    return film;
+  };
 }
 
 export default Film;
