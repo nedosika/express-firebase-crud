6c18c4886b5ec03ba1c375e9bd762ccf3b71b6c4
nedosika <nedosika@gmail.com>
2022-04-03 02:40:37 +0300

    add token prolongation

--- index.js
+++ index.js
@@ -19,10 +19,19 @@ const app = express();
 
 app.use(express.json());
 app.use(cookieParser());
-app.use(cors({
+
+const whitelist = ["http://localhost:3000", "https://express-firebase-crud-bca52.web.app"]
+const corsOptions = {
+    origin: function (origin, callback) {
+        if (!origin || whitelist.indexOf(origin) !== -1) {
+            callback(null, true)
+        } else {
+            callback(new Error("Not allowed by CORS"))
+        }
+    },
     credentials: true,
-    origin: true
-}));
+}
+app.use(cors(corsOptions))
 
 
 app.use("/api", films.router);
