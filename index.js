b1ffe335669c20fd2c26afdb88e407658cc38a2c
nedosika <nedosika@gmail.com>
2022-04-03 00:49:52 +0300

    add token prolongation

--- index.js
+++ index.js
@@ -21,7 +21,7 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(cors({
     credentials: true,
-    origin: '*'
+    origin: ['http://localhost:3000', 'https://express-firebase-crud-bca52.web.app']
 }));
 
 app.use("/api", films.router);
